0de3ef0f4e7929654d6bc5f8dbae17ce2d7ae125
marianna <msamylova@evolution.com>
2025-10-11 20:41:16 +0200

    Add Symbol class

--- src/components/SlotReel.ts
+++ src/components/SlotReel.ts
@@ -6,15 +6,35 @@ export class SlotReel extends PIXI.Container {
 
     constructor() {
         super();
+        this.createInitialSymbols();
     }
 
-    setResult(results: string[]) {
+    public setResult(result: string[]) {
+        // Clear old symbols
         this.removeChildren();
-        this.symbols = results.map((s, i) => {
-            const sym = new Symbol(s);
-            sym.x = i * 120;
-            this.addChild(sym);
-            return sym;
+        this.symbols = [];
+
+        // Create new ones based on backend result
+        result.forEach((s, i) => {
+            const symbol = new Symbol(s);
+            symbol.x = i * 120;
+            symbol.y = 0;
+            this.addChild(symbol);
+            this.symbols.push(symbol);
         });
+    }
+
+    private createInitialSymbols() {
+        // Create 3 random symbols on start
+        const initialSymbols = ["0", "1", "2", "3", "4"];
+        for (let i = 0; i < 3; i++) {
+            const randomSymbol = initialSymbols[Math.floor(Math.random() * initialSymbols.length)];
+            const symbol = new Symbol(randomSymbol);
+
+            symbol.x = i * 120; // horizontal spacing
+            symbol.y = 0;
+            this.addChild(symbol);
+            this.symbols.push(symbol);
+        }
     }
 }
--- src/components/Symbol.ts
+++ src/components/Symbol.ts
@@ -1,7 +1,24 @@
 import * as PIXI from "pixi.js";
 
+
 export class Symbol extends PIXI.Container {
     constructor(symbol: string) {
         super();
+
+        const graphic = new PIXI.Graphics();
+        graphic.fill({ color: 0xffffff * Math.random() }).rect(0, 0, 100, 100);
+
+        const txt = new PIXI.Text({
+            text: symbol,
+            style: {
+                fontSize: 48,
+                fill: 0x000000,
+            },
+        });
+        txt.anchor.set(0.5);
+        txt.x = 50;
+        txt.y = 50;
+
+        this.addChild(graphic, txt);
     }
 }
